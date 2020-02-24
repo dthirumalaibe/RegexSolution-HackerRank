d0b0ed95da7bcf84580cb71e355edb57230c1de2
Snehal Mastud <45584726+snehalmastud@users.noreply.github.com>
2020-02-24 20:32:19 +0530

    Regex Applications!

---
+++ Applications/Find HackerRank/Solution.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import re
+
+for _ in range(int(input())):
+    s=input()
+    if re.search(r'^hackerrank(.*hackerrank)?$',s):
+        print(0)
+    elif re.search(r'^hackerrank',s):
+        print(1)
+    elif re.search(r'hackerrank$',s):
+        print(2)
+    else:
+        print(-1)
